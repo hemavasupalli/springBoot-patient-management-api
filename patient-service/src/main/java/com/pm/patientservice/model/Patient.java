da8c6e78eb40ac55e486168c73115233a2705c94
Hema <hemavasupalli12@gmail.com>
2026-08-01 22:45:10 -0400

    h2 database

--- patient-service/src/main/java/com/pm/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/pm/patientservice/model/Patient.java
@@ -1,6 +1,32 @@
 package com.pm.patientservice.model;
 
-public class Patient {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    private int
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id ;
+    @NotNull
+    private String name;
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+    @NotNull
+    private LocalDate dateOfBirth;
+    @NotNull
+    private LocalDate registeredDate;
+
 }
